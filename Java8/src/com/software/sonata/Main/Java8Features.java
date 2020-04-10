284412c3d6d892479dcaf2f257ca20848b0edb1b
Developer1 <developer@gmail.com>
2020-04-10 21:39:24 +0530

    Removed SPaces

--- Java8/src/com/software/sonata/Main/Java8Features.java
+++ Java8/src/com/software/sonata/Main/Java8Features.java
@@ -21,8 +21,7 @@ public class Java8Features {
 
 		public static void consumerdemo() {
 			List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
-			
-			list1.stream().forEach(t -> System.out.println("print Result from Consumer : " + t));
+         list1.stream().forEach(t -> System.out.println("print Result from Consumer : " + t));
 
 
 
