543bcd638f78863cb496ea6841ef1955cb55c00a
Developer1 <developer@gmail.com>
2020-04-10 19:28:48 +0530

    Test Case is Complted and Executed

--- Java8/src/com/software/sonata/Test/Test.java
+++ Java8/src/com/software/sonata/Test/Test.java
@@ -1,10 +1,14 @@
 package com.software.sonata.Test;
 
-
+import com.software.sonata.Main.Java8Features;
 
 public class Test {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Application is Testing :");
+		Java8Features.predicatedemo();
+		System.out.println("Application is Tested Succesfully");
 	
 	}
 
