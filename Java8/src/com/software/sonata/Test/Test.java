0548c4c8a0b649743b1b0f9e9cb7289f0d86b45c
Developer1 <developer@gmail.com>
2020-04-10 16:33:01 +0530

    Testing Complieted

--- Java8/src/com/software/sonata/Test/Test.java
+++ Java8/src/com/software/sonata/Test/Test.java
@@ -1,9 +1,13 @@
 package com.software.sonata.Test;
 
+import com.software.sonata.Main.Java8Features;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Application Testing Started");
+    Java8Features.predicatedemo();
+	System.out.println("Application Testing Complted");
 
 	}
 
