fb6d79f72a3521d2305e6905b96e67313fe53d85
Developer1 <developer@gmail.com>
2020-04-10 18:40:30 +0530

    Done Changes

--- Java8/src/com/software/sonata/Main/Java8Features.java
+++ Java8/src/com/software/sonata/Main/Java8Features.java
@@ -17,7 +17,6 @@ public class Java8Features {
 	}
 	
 	public static void consumerdemo() {
-		List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
-		list1.stream().filter(t -> t % 2 == 0).forEach(t -> System.out.println("print  Result: " + t));
-	}
+	
+}
 }
