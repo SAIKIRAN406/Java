deec7727947434619d9b7e17596bbb6e75536c0e
Developer1 <developer@gmail.com>
2020-04-10 16:06:18 +0530

    Done The changes Required

--- Java8/src/com/software/sonata/Main/Java8Features.java
+++ Java8/src/com/software/sonata/Main/Java8Features.java
@@ -1,10 +1,18 @@
 package com.software.sonata.Main;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Java8Features {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		predicatedemo();
 
 	}
 
+	public static void predicatedemo() {
+		List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
+		list1.stream().filter(t -> t % 2 == 0).forEach(t -> System.out.println("print  Result: " + t));
+	}
 }
