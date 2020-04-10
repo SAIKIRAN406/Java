c1389a4b7a7172e40e9ef2522f7f938b899c1b1b
Developer1 <developer@gmail.com>
2020-04-10 18:40:30 +0530

    Done Changes

--- Java8/src/com/software/sonata/Test/Test.java
+++ Java8/src/com/software/sonata/Test/Test.java
@@ -1,13 +1,9 @@
 package com.software.sonata.Test;
 
-import com.software.sonata.Main.Java8Features;
-
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Application Testing Started");
-    Java8Features.predicatedemo();
-	System.out.println("Application Testing Started");
+		
 
 	}
 
