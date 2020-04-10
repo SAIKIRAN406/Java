eb7c09a9e16d24e64d1044b6804711828806ef64
Developer1 <developer@gmail.com>
2020-04-10 20:03:20 +0530

    Changes Done

--- Java8/src/com/software/sonata/Main/Java8Features.java
+++ Java8/src/com/software/sonata/Main/Java8Features.java
@@ -8,15 +8,21 @@ public class Java8Features {
 	public static void main(String[] args) {
 
 		predicatedemo();
+	//	consumerdemo();
 
 	}
 
 	public static void predicatedemo() {
 		List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
-		list1.stream().filter(t -> t % 2 == 0).forEach(t -> System.out.println("print  Result: " + t));
+		list1.stream().filter(t -> t % 2 == 0).forEach(t -> System.out.println("print  Result from Predicate: " + t));
 	}
-	
+
 	public static void consumerdemo() {
-	
-}
+		/*
+		 * List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
+		 * list1.stream().forEach(t ->
+		 * System.out.println("print Result from Consumer   : " + t));
+		 */
+
+	}
 }
